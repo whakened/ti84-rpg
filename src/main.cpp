512f8b7fd0cfdd4e4c63ad16cdcec7768d406938
worcestershire sauce <82817571+whakened@users.noreply.github.com>
2022-11-17 10:26:27 -0500

    huh???

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 /*
  *--------------------------------------
- * Program Name:
- * Author:
- * License:
- * Description:
+ * Program Name: TI84RPG
+ * Author: WRCSTRSHR
+ * License: GNU GPLv3
+ * Description: An rpg for the ti84CE
  *--------------------------------------
 */
 
-int main(void)
-{
-    return 0;
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string input;    //declare this variable as a string
+  cin >> input;   //get input from console as variable
+  cout << input;    //output variable value to console
+  return 0;
 }
