eb9b844cd944ae6915b358cf50d3eb941c9b4631
worcestershire sauce <82817571+whakened@users.noreply.github.com>
2022-12-02 13:28:51 -0500

    goood motherfuckin daaaamn

--- CEdev/examples/ti84rpg/src/main.cpp
+++ CEdev/examples/ti84rpg/src/main.cpp
@@ -8,6 +8,7 @@
 */
 
 #include <string.h>
+#include <stdint.h>
 
 #include <ti/getcsc.h>
 #include <ti/tokens.h>
@@ -15,17 +16,17 @@
 
 using namespace ti::literals; // namespace std; otherwise
 
-void os_Output(char text, int row, int column) {
+void os_Output(char text, uint8_t row, uint8_t column) {
 	os_SetCursorPos(row, column);
 	os_PutStrFull(text);
 }
 
-void menu(char options[]) {
+/* void menu(char options[]) {
 	bool entered = false;
 	bool keyPressed = false;
 	uint8_t selected = 0;
 	while (!entered) {
-		for (int i : options) {
+		for (uint8_t i : options) {
 			os_Output(i + ": " + options[i], i, 0);
 		}
 		while (!keyPressed) {
@@ -44,17 +45,41 @@ void menu(char options[]) {
 		}
 		keyPressed = true;
 	} 
+} */
+
+void mainMenu(void) {
+	os_Output("1:Test",0,0);
+	os_Output("2:Other",1,0);
+	bool keyPressed = false;
+	bool entered = false;
+	uint8
+	while (!keyPressed) {
+			uint8_t key = os_GetCSC();
+			if (key != sk_Enter) {
+				if (key == sk_Down && selected > 0) {
+					selected -= 1;
+					keyPressed = true;
+				} else if (key == sk_Up && selected < 2) {
+					selected += 1;
+					keyPressed = true;
+				}
+			} else {
+				entered = true;
+			}
+		}
+	}
 }
 
 int main(void) {
 	os_ClrHome();
 	struct {
-		unsigned int health = 10;
-		unsigned int weapon = 1;
+		uint16_t health = 10;
+		uint8_t weapon = 1;
 		char inventory[14][3];
-	  } playerData;
-	char choices[3][] = {"One", "Two", "Three"};
-	menu(choices);
+	} playerData;
+
+
+
 	/*string input;    //declare this variable as a string
 	cin >> input;   //get input from console as variable
 	cout << input;    //output variable value to console*/
